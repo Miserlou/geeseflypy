b56033113f05a1b29ea80e9fdca59caec9bcc115
bowmanjd <bowmanjd@2912f929-59d1-e70d-65d7-871aca5feeb2>
2010-09-30 20:06:33 +0000

    finished

--- geesefly/geesefly.py
+++ geesefly/geesefly.py
@@ -10,12 +10,17 @@
 #
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+#  implied. See the License for the specific language governing
+#  permissions and limitations under the License.
 
 from skein import *
 import operator
+import zlib
+import binascii
+
+class AuthenticationError(StandardError):
+    pass
 
 # The following will be used as a prefix for ciphertext encoded with the
 # encrypt function. The "encode" method is used so that it will be a
@@ -31,7 +36,8 @@ def encrypt(data, key):
         encrypt = True
         salt = Skein512Random().getbytes(16)
 
-    hashed = Skein512(msg=key, digest_bits=1024, key=salt, block_type='nonce').final()
+    hashed = Skein512(msg=key, digest_bits=1024, key=salt,
+                      block_type='nonce').final()
     key = hashed[:64]
     iv = hashed[64:]
     tf = Threefish512(key)
@@ -40,20 +46,27 @@ def encrypt(data, key):
     if encrypt:
         output = ciphertext_prefix + salt
         blocks, remainder = divmod(len(data), 64)
-        for block in (bytes2words(data[i*64:(i+1)*64]) for i in xrange(blocks)):
-            previous_block = tf.encrypt_block(list(imap(operator.xor, previous_block, block)))
+        for block in (bytes2words(data[i*64:(i+1)*64])
+                      for i in xrange(blocks)):
+            previous_block = tf.encrypt_block(list(imap(operator.xor,
+                                                        previous_block,
+                                                        block)))
             output += words2bytes(previous_block)
 
         pad_val = 64 - remainder
         pad = struct.pack("B", pad_val) * pad_val
         if remainder:
             pad = data[-remainder:] + pad
-        block = list(imap(operator.xor, previous_block, bytes2words(pad)))
+        block = list(imap(operator.xor, previous_block,
+                          bytes2words(pad)))
         output += words2bytes(tf.encrypt_block(block))
     else:
         output = empty_bytes
-        for block in (bytes2words(data[i*64:(i+1)*64]) for i in xrange(len(data)//64)):
-            output += words2bytes(list(imap(operator.xor, previous_block, tf.decrypt_block(block))))
+        for block in (bytes2words(data[i*64:(i+1)*64])
+                      for i in xrange(len(data)//64)):
+            output += words2bytes(list(imap(operator.xor,
+                                            previous_block,
+                                            tf.decrypt_block(block))))
             previous_block = block
 
         output = output.rstrip(output[-1:])
@@ -63,38 +76,52 @@ def encrypt(data, key):
 def compress_encrypt_auth(data, key):
     if data.startswith(ciphertext_prefix):
         encrypt = False
-        mac = data[16:80]
+        received_mac = data[16:80]
         salt = data[80:96]
-        data = data[32:]
+        data = data[96:]
     else:
         encrypt = True
         salt = Skein512Random().getbytes(16)
+        data = zlib.compress(data)
 
-    hashed = Skein512(msg=key, digest_bits=1024, key=salt, block_type='nonce').final()
+    hashed = Skein512(msg=key, digest_bits=1280, key=salt,
+                      block_type='nonce').final()
     key = hashed[:64]
-    iv = hashed[64:]
+    iv = hashed[64:128]
+    mac_key = hashed[128:]
     tf = Threefish512(key)
-    
+
     previous_block = bytes2words(iv)
     if encrypt:
-        output = ciphertext_prefix + salt
+        output = empty_bytes
         blocks, remainder = divmod(len(data), 64)
-        for block in (bytes2words(data[i*64:(i+1)*64]) for i in xrange(blocks)):
-            previous_block = tf.encrypt_block(list(imap(operator.xor, previous_block, block)))
+        for block in (bytes2words(data[i*64:(i+1)*64])
+                      for i in xrange(blocks)):
+            previous_block = tf.encrypt_block(list(imap(operator.xor,
+                                                        previous_block,
+                                                        block)))
             output += words2bytes(previous_block)
 
         pad_val = 64 - remainder
         pad = struct.pack("B", pad_val) * pad_val
         if remainder:
             pad = data[-remainder:] + pad
-        block = list(imap(operator.xor, previous_block, bytes2words(pad)))
+        block = list(imap(operator.xor, previous_block,
+                          bytes2words(pad)))
         output += words2bytes(tf.encrypt_block(block))
+        output = ciphertext_prefix +  Skein512(msg=output, key=mac_key).final() + salt + output
+
     else:
+        if received_mac != Skein512(msg=data, key=mac_key).final():
+            raise AuthenticationError
         output = empty_bytes
-        for block in (bytes2words(data[i*64:(i+1)*64]) for i in xrange(len(data)//64)):
-            output += words2bytes(list(imap(operator.xor, previous_block, tf.decrypt_block(block))))
+        for block in (bytes2words(data[i*64:(i+1)*64])
+                      for i in xrange(len(data)//64)):
+            output += words2bytes(list(imap(operator.xor,
+                                            previous_block,
+                                            tf.decrypt_block(block))))
             previous_block = block
 
-        output = output.rstrip(output[-1:])
+        output = zlib.decompress(output.rstrip(output[-1:]))
 
     return output
